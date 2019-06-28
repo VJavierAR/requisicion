75b7ff73a00c0e211d3317697c7dfac549e09874
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2019-06-28 00:40:44 -0500

    fff

--- requisicion/models/models.py
+++ requisicion/models/models.py
@@ -12,7 +12,7 @@ class product_requisicion(models.Model):
 class requisicion(models.Model):
     _name = 'requisicion.requisicion'
     _description='Requisicion'
-    _inherit = ['mail.thread', 'utm.mixin', 'rating.mixin', 'mail.activity.mixin']
+    _inherit = ['mail.thread']
     name = fields.Char()
     area = fields.Selection([('Ventas','Ventas'),('Almacen','Almacen'), ('Mesa de Ayuda','Mesa de Ayuda')])
     fecha_prevista=fields.Datetime()
