959e1c70bf199fe7e5cfbd17dfb072f0553baf5a
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2019-07-19 14:53:24 -0500

    pytesseract

--- requisicion/models/models.py
+++ requisicion/models/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+import pytesseract
 class product_requisicion(models.Model):
     _name = 'product.rel.requisicion'
     _description='Rel requisiocion'
